27298922373f0154bf34e2f538ada00576b0b7a5
jlark <joseph@dictanova.com>
2015-06-23 16:59:53 +0200

    sync

--- latexupdate.py
+++ latexupdate.py
@@ -7,5 +7,13 @@ class LatexupdateCommand(sublime_plugin.WindowCommand):
 		filepath = self.window.active_view().file_name()
 		filedir  = os.path.dirname(filepath)
 		if filepath[-4:] == '.tex':
+			os.chdir(filedir)
 			self.window.active_view().run_command('save')
-			subprocess.call(shlex.split('pdflatex -output-directory=\''+ filedir + '\' ' + filepath + ' > /dev/null'))
+			# subprocess.call(shlex.split('pdflatex -input-directory=\''+ filedir + '\' ' + '-output-directory=\''+ filedir + '\' ' + filepath + ' > /dev/null'))
+			subprocess.call(shlex.split('pdflatex ' + filepath + ' > /dev/null'))
+			with open(filepath[:-4] + '.log', 'r') as f:
+				# read_data = f.read()
+				for line in f:
+					if 'LaTeX Error' in line:
+						print(line)
+			f.closed
